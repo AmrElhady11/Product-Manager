7107207a685b10abd8cdd929042ef0f352b69fa4
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-29 20:05:44 +0300

    adding new custom method to ProductRepository

--- src/main/java/com/ProductManager/dao/ProductRepository.java
+++ src/main/java/com/ProductManager/dao/ProductRepository.java
@@ -1,18 +1,17 @@
-package com.amazon_El8alaba.dao;
+package com.ProductManager.dao;
 
-import com.amazon_El8alaba.entity.ProductDetailsEntity;
-import com.amazon_El8alaba.entity.ProductEntity;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import com.ProductManager.entity.ProductDetailsEntity;
+import com.ProductManager.entity.ProductEntity;
 
 import java.util.List;
 
 public interface ProductRepository  {
-
+    void save(ProductEntity product);
     ProductEntity insert(ProductDetailsEntity productDetails);
     ProductEntity findById(int id);
     void deleteById(int id);
     void update(ProductDetailsEntity productDetails);
     List<ProductEntity> findAll();
     ProductDetailsEntity findProductDetailsById(int id);
+    ProductDetailsEntity findProductDetailsByProductId(int productId);
 }
